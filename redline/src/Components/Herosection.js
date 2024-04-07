a508be719fa17674af6ae2e7bcf092bd89b261e1
elrmam <greatdream0000@gmail.com>
2024-04-07 07:26:22 +0200

    update marigin

--- redline/src/Components/Herosection.js
+++ redline/src/Components/Herosection.js
@@ -3,7 +3,7 @@ function Herosectin(){
     return(
             <div className="container-fluid background" style={{ height: "33rem" }}>
                 <div className="row">
-                    <div className="col-12 ms-5 mt-5">
+                    <div className="col-12 ps-5 mt-5">
                         <div className="text-start redlineheader">
                             <span className="herored">REDLINE</span> 
                             <span>&nbsp;</span> 
