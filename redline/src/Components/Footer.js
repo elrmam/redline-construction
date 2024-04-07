6db2de311cbf23f9055fceb5eefcee46e22a964d
elrmam <greatdream0000@gmail.com>
2024-04-07 07:29:21 +0200

    udpate

--- redline/src/Components/Footer.js
+++ redline/src/Components/Footer.js
@@ -65,7 +65,7 @@ function Footer() {
         <div className='col-lg-3 col-md-6 col-sm-12'>
           <h5 className='text-white'>Location</h5>
           <iframe title="Company Location Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.2242275720527!2d31.274030!3d29.982590!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zYOUR_COMPANY_ADDRESS_HERE!5e0!3m2!1sen!2sus!4v1641534423335!5m2!1sen!2sus" 
-              width="300" height="200" style={{ border:'0' }} allowfullscreen loading="lazy"></iframe>
+              width="290" height="200" style={{ border:'0' }} allowfullscreen loading="lazy"></iframe>
         </div>
       </div>
       <div className='row'>
