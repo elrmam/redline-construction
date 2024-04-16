63b686b23570609fc5cca269bd20a1c49c6f7def
Hossam Hassan <hossam.sahss120@gmail.com>
2024-04-16 19:39:15 +0200

    New Structure after Ahmed Design

---
+++ src/Components/About.js
@@ -0,0 +1,24 @@
+function About(){
+
+    return(
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6 col-sm-12 text-start">
+                        <h5 className="text-danger">ABOUT US</h5>
+                        <h1>WHY REDLINE</h1>
+                        <p> REDLINE CONSTRUCTION Is A Leading Company In
+                            Engineering , Constructions And Finishing Works .
+                            It Has Many Works That Has Been Implemented In
+                            Several Places In Egypt .
+                        </p>
+                        <br/><button type="button" className="bg-black text-white w-25 py-2">LEARN MORE</button>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <img src="/aboutus.jpg" className="card-img-top" alt="discribsion"  />
+                    </div>
+                </div>
+            </div>
+    )
+
+}
+export default About;
---
+++ src/Pages/Home.js
@@ -0,0 +1,23 @@
+import About from "../Components/About";
+import Goals from "../Components/Goals";
+import Herosectin from "../Components/Herosection";
+import License from "../Components/License";
+import Projects from "../Components/Projects";
+import Services from "../Components/Services";
+
+
+function Homepage(){
+
+    return(
+        <>
+            <Herosectin/>
+            <Services/>
+            <About/>
+            <Goals/>
+            <Projects/>
+            <License/>
+        </>
+    )
+}
+
+export default Homepage;
