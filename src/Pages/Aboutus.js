1083d9d4d14fb5234384a995696fda204debed80
Hossam Hassan <99551852+HossamHassan70@users.noreply.github.com>
2024-04-20 21:42:48 +0200

    add About Us Page

---
+++ src/Pages/Aboutus.js
@@ -0,0 +1,54 @@
+function Aboutus() {
+
+    return (
+        <div className="container my-5">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12 text-start">
+                    <h1>About Us</h1>
+                    <h5> <span style={{"color":"red"}}>RED</span>LINE COMPANY <br/>
+                    </h5>
+                    <h5>Welcome to Redline Company, 
+                        where excellence meets innovation in engineering, 
+                        construction, and finishing works. 
+                        As a premier company in our field, 
+                        we pride ourselves on delivering superior quality and 
+                        unmatched expertise to projects throughout Egypt.
+                    </h5>
+                    <h6>At Redline Company, our commitment to excellence 
+                        is evident in every aspect of our work. 
+                        From the initial concept to the final touches, 
+                        we strive for perfection in every detail. 
+                        Our team of experienced professionals brings a wealth 
+                        of knowledge and skill to every project, 
+                        ensuring that we meet and exceed the expectations 
+                        of our clients.
+                    </h6>
+                    <h6>What sets Redline Company apart is our dedication 
+                        to delivering exceptional results on time and within 
+                        our budget. With a proven track record of successful 
+                        projects across various locations in Egypt, 
+                        we have earned a reputation for reliability, integrity, 
+                        and professionalism.
+                    </h6>
+                    <h6>At Redline Company, we understand that every project is unique, 
+                        which is why we tailor our services to meet the specific requirements 
+                        of each client. We work closely with our clients from the initial 
+                        planning stages through to completion, ensuring clear communication 
+                        and collaboration every step of the way.
+                    </h6>
+                    <h6>With a focus on quality, innovation, and customer satisfaction, 
+                        Redline Company is your trusted partner for all your engineering, 
+                        construction, and finishing needs in Egypt. 
+                        Contact us today to learn more about how we can bring your 
+                        vision to life.
+                    </h6>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12 mt-5">
+                    <img src="/aboutus.jpg" className="imghome rounded" alt="discribsion" />
+                </div>
+            </div>
+        </div>
+    )
+
+}
+export default Aboutus;
