f2377a758fdb20c73b4bf4b1992c95503b391b3a
elrmam <ahmedredaelrmam@gmail.com>
2024-05-16 23:00:18 +0300

    herotxt

--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -4,12 +4,12 @@ function Herosectin(){
             <div className="container-fluid background" style={{ height: "33rem" }}>
                 <div className="row">
                     <div className="col-12 ps-5 mt-5">
-                        <div className="text-center heromarigin">
-                            <h2 className="fs-1">
+                        <div className="text-center heromargin">
+                            <h1 className="fs-1 herotxtstart">
                                 <span className='textred'>RED</span>
                                 <span className='text-white'>LINE</span>
-                            </h2>
-                            <h1 className="text-white herospaces">CONSTUCTIONS</h1> 
+                            </h1>
+                            <h1 className="text-white herospaces herotxtstart">CONSTUCTIONS</h1> 
                         </div>
                     </div>
                 </div>
